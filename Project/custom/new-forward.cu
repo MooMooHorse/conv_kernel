9995ac99821b1093d88259de904e9724b7dad00b
Hao Ren <haor.20@intl.zju.edu.cn>
2023-11-27 14:12:50 +0800

    merge baseline in

--- Project/custom/new-forward.cu
+++ Project/custom/new-forward.cu
@@ -12,10 +12,90 @@
 
 #define IS_TEST_1(B,M,C,H,W,K,S)  (B==1 && M == 3 && C == 3 && H == 224 && W == 224 && K == 3 && S == 1)
 
+#if (CUR_VERSION == IMPL_BASELINE)
 
-#if (CUR_VERSION == IMPL_SHARED_UNROLLING)
+/**
+ *  const int H_out = (H - K)/S + 1;
+ *  const int W_out = (W - K)/S + 1;
+    dim3 dimGrid(ceil(W_out/float(BLOCK_SIZE)), ceil(H_out/float(BLOCK_SIZE)), M * B);
+ *  dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
+ * It means for each image and each feature map, we have a grid of blocks. since we have strides, 
+ * we divide works after strides into different blocks.
+*/
+__global__ void conv_forward_kernel(float *output, const float *input, const float *mask, 
+    const int B, const int M, const int C, const int H, const int W, const int K,const int S) {
+    /*
+    Modify this function to implement the forward pass described in Chapter 16.
+    We have added an additional dimension to the tensors to support an entire mini-batch
+    The goal here is to be correct AND fast.
 
-__global__ void conv_forward_kernel_unrolling_shared(float *output, const float *input, const float *mask, 
+    Function paramter definitions:
+    output - output
+    input - input
+    mask - convolution kernel
+    B - batch_size (number of images in x)
+    M - number of output feature maps
+    C - number of input feature maps
+    H - input height dimension
+    W - input width dimension
+    K - kernel height and width (K x K)
+    S - stride step length
+    */
+
+
+    const int H_out = (H - K)/S + 1;
+    const int W_out = (W - K)/S + 1;
+
+
+    // We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
+    // An example use of these macros:
+    // float a = in_4d(0,0,0,0)
+    // out_4d(0,0,0,0) = a
+
+    #define out_4d(i3, i2, i1, i0) output[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+    #define in_4d(i3, i2, i1, i0) input[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+    #define mask_4d(i3, i2, i1, i0) mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
+    // Insert your GPU convolution kernel code here
+
+    // pick a way to divide work for image and feature map
+    int b = blockIdx.x % B;
+    int m = blockIdx.y % M;
+
+    int bx = blockIdx.x / B;
+    int by = blockIdx.y / M;
+
+    int w = bx * blockDim.x + threadIdx.x;
+    int h = by * blockDim.y + threadIdx.y;
+
+    // load to shared memory will be useless if stride size is big.
+    // so we directly use global memory
+
+    float sum = 0.0f;
+
+    for (int c = 0; c < C; c++) {
+        for (int p = 0; p < K; p++) {
+            for (int q = 0; q < K; q++) {
+                if (h * S + p < H && w * S + q < W) {
+                    sum += in_4d(b, c, h * S + p, w * S + q) * mask_4d(m, c, p, q);
+                }
+            }
+        }
+    }
+
+    if(h < H_out && w < W_out)
+        out_4d(b, m, h, w) = sum;
+
+
+    #undef out_4d
+    #undef in_4d
+    #undef mask_4d
+}
+
+
+#elif (CUR_VERSION == IMPL_SHARED_UNROLLING)
+
+__global__ void conv_forward_kernel(float *output, const float *input, const float *mask, 
     const int B, const int M, const int C, const int H, const int W, const int K,const int S) {
 	//@@ Insert code to implement matrix multiplication here
 	//@@ You have to use shared memory for this MP
@@ -149,7 +229,7 @@ __global__ void im2col_kernel(float* output, const float *input, float* mask_o, 
 }
 
 #elif (CUR_VERSION == IMPL_UNROLLING)
-__global__ void conv_forward_kernel_unrolling(float *output, const float *input, const float *mask, 
+__global__ void conv_forward_kernel(float *output, const float *input, const float *mask, 
     const int B, const int M, const int C, const int H, const int W, const int K,const int S) {
 	//@@ Insert code to implement matrix multiplication here
 	//@@ You have to use shared memory for this MP
@@ -297,19 +377,20 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, co
 
     // Set the kernel dimensions and call the kernel
 
-    // const int H_out = (H - K)/S + 1;
-    // const int W_out = (W - K)/S + 1;
+#if (CUR_VERSION == IMPL_BASELINE)
+
+    const int H_out = (H - K)/S + 1;
+    const int W_out = (W - K)/S + 1;
 
     // Allocate device memory for the output, input and mask
-//     cudaMalloc(device_output_ptr, B * M * H_out * W_out * sizeof(float));
+    cudaMalloc(device_input_ptr, B * C * H * W * sizeof(float));
+    cudaMalloc(device_mask_ptr, M * C * K * K * sizeof(float));
+    cudaMalloc(device_output_ptr, B * M * H_out * W_out * sizeof(float));
 
-// // #if (CUR_VERSION == IMPL_BASELINE)
-//     cudaMalloc(device_input_ptr, B * C * H * W * sizeof(float));
-//     cudaMalloc(device_mask_ptr, M * C * K * K * sizeof(float));
+    cudaMemcpy(*device_input_ptr, host_input, B * C * H * W * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(*device_mask_ptr, host_mask, M * C * K * K * sizeof(float), cudaMemcpyHostToDevice);
 
-//     cudaMemcpy(*device_input_ptr, host_input, B * C * H * W * sizeof(float), cudaMemcpyHostToDevice);
-//     cudaMemcpy(*device_mask_ptr, host_mask, M * C * K * K * sizeof(float), cudaMemcpyHostToDevice);
-
+#elif (CUR_VERSION == IMPL_SHARED_UNROLLING)
 
     const int H_out = (H - K)/S + 1;
     const int W_out = (W - K)/S + 1;
@@ -339,7 +420,8 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, co
     free(host_unroll_mask);
     *device_input_ptr = device_unroll_input; // now the input pointer has been swapped to unrolled input
     *device_mask_ptr = device_unroll_mask; // now the mask pointer has been swapped to unrolled mask
-    
+#endif 
+
     cudaError_t error = cudaGetLastError();
     if(error != cudaSuccess)
     {
@@ -366,7 +448,7 @@ const float *device_mask, const int B, const int M, const int C, const int H, co
 
     
 
-    conv_forward_kernel_unrolling<<<dimGrid, dimBlock>>>(device_output, device_input, 
+    conv_forward_kernel<<<dimGrid, dimBlock>>>(device_output, device_input, 
         device_mask, B, M, C, H, W, K, S);
 
     cudaDeviceSynchronize();
